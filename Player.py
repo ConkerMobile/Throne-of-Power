d2c78346a7bab079aa5f659d2f06fa2c09e3ee8a
qingy2019 <66195939+qingy2019@users.noreply.github.com>
2021-02-13 19:15:48 -0800

    WOOOHOOO!

--- Player.py
+++ Player.py
@@ -1,12 +1,37 @@
 import random
 class Player():
-    def __init__(self):
-        Possible_Moves = ["Punch", "Sword Slice", "Knife Swing", "Thundering Earthquake","Hypnosis","Electric Bolt","Nuclear Hit", "Lightsaber Stab"]
-        Move_damage = {"Punch":70, "Sword Slice":80, "Knife Swing": 100, "Thundering Earthquake":150,"Hypnosis":200,"Electric Bolt":170,"Nuclear Hit": 180, "Lightsaber Stab": 200}
+    def __init__(self, name):
+        Possible_Moves = ["Punch", "Sword Slice", "Knife Swing", "Earthquake","Hypnosis","Electric Bolt","Nuclear Hit", "Lightsaber Stab"]
+        Move_damage = {"Punch":70, "Sword Slice":80, "Knife Swing": 100, "Earthquake":150,"Hypnosis":200,"Electric Bolt":170,"Nuclear Hit": 180, "Lightsaber Stab": 200}
+        self.wins = 0
         self.health = 500
+        self.battle_health = self.health
         self.moves = []
-        self.damage = 0
+        self.damage = {}
+        self.max_damage = 0
+        self.name = name
         for i in range(2):
-            self.moves.append(random.choice(Possible_Moves))
-            self.damage
-        
+            choice = random.choice(Possible_Moves)
+            self.moves.append(choice)
+            self.max_damage+=Move_damage[choice]
+            self.damage[choice] = Move_damage[choice]
+    def upgrade(self):
+        upgraded = False
+        """Upgrades health and damage when you have a certain amount of wins"""
+        if (self.wins >= 3 and self.wins < 5):
+            self.health = 600
+            upgraded = True
+        elif (self.wins >= 5 and self.wins < 7):
+            self.health = 900
+            upgraded = True
+        elif (self.wins >= 7 and self.wins < 10):
+            self.health = 1100
+            upgraded = True
+        elif (self.wins >= 10):
+            self.health = 1300
+            upgraded = True
+        self.battle_health = self.health
+        if upgraded == True:
+            for i in self.moves:
+                self.damage[i]+=50
+            self.max_damage +=100
